8156bdea9cd627e8cf50f3e836b95d6921801064
YoussefBasher <youssefbasher999@gmail.com>
2026-08-03 10:31:52 +0300

    Implement execute_action function for candidate decisions

---
+++ Rule Based Model/actions.py
@@ -0,0 +1,14 @@
+def execute_action(decision, candidate):
+    print(f"\nCandidate:{candidate.get('name')}")
+    if decision == "ACCEPT":
+        print ("Candidate Accepted")
+        print("Action: Schedule Interview")
+    elif decision == "REJECT":
+        print("Candidate Rejected")
+        print("Action: Send Rejection email")
+    elif decision == "IDLE":
+        print("Candidate needs Human Review")
+        print("Action: Forward to Recruiter")
+    else:
+        print(f"Unkown decision{decision}")
+        
